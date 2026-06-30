6f107db2811461538c2fc77a69b2da9d2172ca7f
Navaras-g <shresthanavaras345@gmail.com>
2026-06-30 20:25:54 +0545

    skills mart and priority mart added

--- Warehousing/build_dw_marts.sql
+++ Warehousing/build_dw_marts.sql
@@ -6,3 +6,9 @@
 
 -- Step 3: Mart - Create flat mart (denormalized table)
 .read create_flat_mart.sql
+
+-- Step 4: Mart - Create skills demand mart
+.read create_skills_mart.sql
+
+-- Step 5: Mart - Create priority mart
+.read create_priority_mart.sql
